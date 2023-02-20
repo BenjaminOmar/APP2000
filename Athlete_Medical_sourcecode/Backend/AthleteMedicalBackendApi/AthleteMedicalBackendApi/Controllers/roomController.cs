a3f41bda66ff7639aefe1f79b68ca4620704de3d
Warsame <warsame9194@outlook.com>
2023-02-21 00:29:23 +0100

    made GetHttp for rooms
    couldn´t test it out the the api swagger, because of dependcy error.

---
+++ Athlete_Medical_sourcecode/Backend/AthleteMedicalBackendApi/AthleteMedicalBackendApi/Controllers/roomController.cs
@@ -0,0 +1,24 @@
+﻿using AthleteMedicalBackendApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AthleteMedicalBackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class roomController : Controller
+    {
+        private readonly App2000Context _context;
+
+        public roomController(App2000Context context) => _context = context;   
+
+
+        [HttpGet]
+        public async Task<IEnumerable<Room>> Get()
+        {
+            return await _context.Rooms.ToListAsync();
+        }
+
+    }
+}
