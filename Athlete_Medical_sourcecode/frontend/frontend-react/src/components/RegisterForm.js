1cf49fb5b95e105684adc4644b7a4f6e0ac49c92
ellenkh <92035794+ellenkh@users.noreply.github.com>
2023-03-02 20:06:28 +0100

    Update RegisterForm.js

--- Athlete_Medical_sourcecode/frontend/frontend-react/src/components/RegisterForm.js
+++ Athlete_Medical_sourcecode/frontend/frontend-react/src/components/RegisterForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button, Alert, Card } from "react-bootstrap";
+import { Form, Button, Alert, Card, Modal } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const RegisterForm = () => {
@@ -18,41 +18,42 @@ const RegisterForm = () => {
 	const [confirmPassword, setConfirmPassword] = useState("");
 	const [errorMessage, setErrorMessage] = useState("");
 	const [isFlipped, setIsFlipped] = useState(false);
+	const [showModal, setShowModal] = useState(false);
 
-	const apiUrl = process.env.RAECT_APP_API_URL;
+	const apiUrl = process.env.REACT_APP_API_URL;
 
 	//Handle form submit event
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		if (
-			password !== confirmPassword ||
-			!/[a-åA-Å]/.test(password) ||
-			/[0-9]/.test(password) ||
-			password.length < 8
-		) {
+		if (!/[a-zA-ZæøåÆØÅ]/.test(password) ||
+			!/[0-9]/.test(password)) {
 			setErrorMessage(
-				"Passordet må inneholde mist 8 karakterer, inkludert minst en bokstav og ett nummer"
-			);
+				"Passordet må inneholde mist 8 karakterer, inkludert minst en bokstav og ett nummer");
+			setShowModal(true);
 			return;
 		}
 
 		if (password !== confirmPassword) {
 			setErrorMessage("Passordene er ikke like");
+			setShowModal(true);
 			return;
 		}
+
+
+		//Her må du få inn flere feilmeldinger.... Brukernavn, passord, personnummer, postnummer
 
 		//Send under input to server to check if user already exists
 		fetch(apiUrl + "/User/check", {
 			method: "POST",
-			headers: { "Content Type": "application/json" },
+			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify({ username, ssn }),
 		})
 			.then((response) => {
 				if (response.ok) {
 					//User does not exist, submit registration
-					return fetch(apiUrl + "User/register", {
+					return fetch(apiUrl + "/User/register", {
 						method: "POST",
-						headers: { "Content Type": "application/json" },
+						headers: { "Content-Type": "application/json" },
 						body: JSON.stringify({
 							username: username.trim(),
 							firstName: firstName.trim(),
@@ -74,10 +75,10 @@ const RegisterForm = () => {
 			})
 			.then((response) => response.json())
 			.then((data) => {
-				//Registration successfully, show message and user information.
 				setErrorMessage("");
-				alert("Bruker registrert vellykket. Informasjon: ");
-			})
+				setShowModal(true);
+				// setModalBody(`Bruker registrert vellykket. Informasjon: ${JSON.stringify(data)}`);
+			  })
 			.catch((error) => console.error(error));
 	};
 	//flipp card function
@@ -88,28 +89,37 @@ const RegisterForm = () => {
 	const handleClose = () => {
 		setErrorMessage("");
 	};
+
 	// Function that makes a GET request to the server with the postal code parameter, and updates the postal location in the state
 	const handleZipcodeChange = (event) => {
-		const zipcode = event.targe.value;
+		const zipcode = event.target.value;
 		if (zipcode.length === 4) {
-			fetch(apiUrl + "/city/getcity")
-				.then((response) => {
-					if (response.ok) {
-						return response.json();
-					} else {
-						throw new Error("Ugyldig postnummer");
-					}
-				})
-				.then((data) => {
-					setCity(data.city);
-				})
-				.catch((error) => {
-					setErrorMessage(error.message);
-				});
+	  	fetch(apiUrl + "/city/getcity", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ zipcode }),
+	  	})
+		.then((response) => {
+		  if (response.ok) {
+			return response.json();
+		  } else {
+			throw new Error("Ugyldig postnummer");
+		  }
+		})
+		.then((data) => {
+		  	setCity(data.city);
+		  	setErrorMessage("");
+		})
+		.catch((error) => {
+		  	setCity("");
+		  	setErrorMessage("Ugyldig postnummer");
+		  	setShowModal(true);
+		});
 		} else {
-			setCity("");
+	  		setCity("");
+	  		setErrorMessage("");
 		}
-	};
+  	};
 
 	return (
 		<div
@@ -117,14 +127,21 @@ const RegisterForm = () => {
 			style={{ paddingTop: "0px", position: "relative" }}>
 			{/*A Card component from bootstrap-react that is used to display the Register form.*/}
 			<Form onSubmit={handleSubmit}>
+				{/*Display error message if there is one*/}
 				{errorMessage && (
-					<Alert variant="danger" onClose={handleClose} dismissible>
-						<Alert.Heading>Feil i skjema!</Alert.Heading>
-						<p>{errorMessage}</p>
-					</Alert>
-				)}
+  					<Modal show={showModal} onHide={() => setShowModal(false)}>
+    					<Modal.Header style={{backgroundColor: '#ff9d80'}} closeButton>
+      						<Modal.Title>Feil</Modal.Title>
+    					</Modal.Header>
+    					<Modal.Body  >{errorMessage}</Modal.Body>
+    					<Modal.Footer>
+      						<Button style={{backgroundColor: "#0050B1"}} variant="secondary" onClick={handleClose}>
+        						Lukk
+      						</Button>
+    					</Modal.Footer>
+  					</Modal>
+				)}					
 				<Card style={{ width: "500px", marginBottom: "50px" }}>
-					{/*Display error message if there is one*/}
 					<Card.Header>
 						<h3> Registrer Bruker</h3>
 					</Card.Header>
@@ -189,13 +206,14 @@ const RegisterForm = () => {
 							/>
 						</Form.Group>
 						<Form.Group controlId="ssn" style={{ marginBottom: "15px" }}>
-							<Form.Label>Personnummer, 12 siffer:</Form.Label>
+							<Form.Label>Personnummer, 12 tall:</Form.Label>
 							<Form.Control
-								type="number"
+								type="text"
 								placeholder="Personnummer"
 								value={ssn}
 								onChange={(e) => setSsn(e.target.value)}
 								maxLength="12"
+								minLength="12"
 								required
 							/>
 						</Form.Group>
@@ -210,12 +228,12 @@ const RegisterForm = () => {
 							/>
 						</Form.Group>
 						<Form.Group style={{ marginBottom: "15px" }} controlId="zipCode">
-							<Form.Label>Postnummer:</Form.Label>
+							<Form.Label>Postnummer, 4 tall:</Form.Label>
 							<Form.Control
 								type="text"
 								placeholder="Postnummer"
 								// value={zipCode}
-								onChange={handleZipcodeChange}
+								//onChange={handleZipcodeChange}
 								maxLength="4"
 								required
 							/>
@@ -231,7 +249,9 @@ const RegisterForm = () => {
 							/>
 						</Form.Group>
 						<Form.Group controlId="password" style={{ marginBottom: "15px" }}>
-							<Form.Label>Passord:</Form.Label>
+							<Form.Label style={{ marginBottom: "-5px" }}>Passord:
+								<p style={{ fontSize: '10px' }} > (minimum 8 karakterer, derav minimum en bokstav og ett tall)</p>
+							</Form.Label>
 							<Form.Control
 								type="password"
 								placeholder="Passord"
@@ -279,7 +299,8 @@ const RegisterForm = () => {
 						</Form.Group>
 					</Card.Body>
 				</Card>
-			</Form>
+			</Form>			
+
 		</div>
 	);
 };
