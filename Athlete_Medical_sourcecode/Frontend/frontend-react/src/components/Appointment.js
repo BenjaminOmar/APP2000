3ebdc14884609fb9977d4d4a0a97025e2eab5d15
gunillag <83660232+gunillag@users.noreply.github.com>
2023-03-21 00:52:33 +0100

    booking

--- Athlete_Medical_sourcecode/Frontend/frontend-react/src/components/Appointment.js
+++ Athlete_Medical_sourcecode/Frontend/frontend-react/src/components/Appointment.js
@@ -7,7 +7,8 @@ function Appointment() {
   const [selectedSpecialist, setSelectedSpecialist] = useState('');
   const [availableAppointments, setAvailableAppointments] = useState([]);
   const [selectedAppointment, setSelectedAppointment] = useState('');
-  
+  const [appointmentsBySpecialist, setAppointmentsBySpecialist] = useState({});
+
   useEffect(() => {
     axios.get('https://localhost:7209/api/User/specialists')
       .then(response => {
@@ -19,12 +20,18 @@ function Appointment() {
   }, []);
 
   const handleSpecialistChange = event => {
-    setSelectedSpecialist(event.target.value);
+    const specialistId = event.target.value;
+    setSelectedSpecialist(specialistId);
     setSelectedAppointment('');
-    if (event.target.value) {
-      axios.get(`https://localhost:7209/api/appointment/available?specialistId=${event.target.value}&isAvailable=1`)
+    if (specialistId) {
+      axios.get(`https://localhost:7209/api/appointment/available?specialistId=${specialistId}&isAvailable=1`)
         .then(response => {
-          setAvailableAppointments(response.data);
+          const specialistAppointments = response.data.filter(appointment => appointment.specialist.id === parseInt(specialistId));
+          setAvailableAppointments(specialistAppointments);
+          setAppointmentsBySpecialist(prevState => ({
+            ...prevState,
+            [specialistId]: specialistAppointments
+          }));
         })
         .catch(error => {
           console.log(error);
@@ -33,17 +40,14 @@ function Appointment() {
       setAvailableAppointments([]);
     }
   };
-  
 
   const handleAppointmentChange = event => {
     setSelectedAppointment(event.target.value);
   };
 
   const bookAppointment = appointmentId => {
-    
     axios.post('https://localhost:7209/api/appointment/book', {
       appointmentId
-     
     })
       .then(response => {
         alert('Appointment booked successfully!');
@@ -63,38 +67,40 @@ function Appointment() {
 
   return (
     <div className="appointment-container">
-    <div className="appointment-system">
-    <div>
-      <h1>Timebestilling</h1>
-      <form onSubmit={handleBooking}>
+      <div className="appointment-system">
         <div>
-          <label>Spesialist</label>
-          <select value={selectedSpecialist} onChange={handleSpecialistChange}>
-            <option value="">Velg spesialist</option>
-            {specialists.map(specialist => (
-              <option key={specialist.id} value={specialist.id}>
-                   {`${specialist.firstName} ${specialist.middleName} ${specialist.lastName}`}
-              </option>
-            ))}
-          </select>
+          <h1>Timebestilling</h1>
+          <form onSubmit={handleBooking}>
+            <div>
+              <label>Spesialist</label>
+              <select value={selectedSpecialist} onChange={handleSpecialistChange}>
+                <option value="">Velg spesialist</option>
+                {specialists.map(specialist => (
+                  <option key={specialist.id} value={specialist.id}>
+                    {`${specialist.firstName} ${specialist.middleName} ${specialist.lastName}`}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            {selectedSpecialist && (
+              <div specialist={selectedSpecialist}>
+                <label>{`Velg avtaletidspunkt med ${selectedAppointment ? appointmentsBySpecialist[selectedSpecialist].find(appointment => appointment.id === selectedAppointment).specialist.firstName : ''}`}</label>
+                <select value={selectedAppointment} onChange={handleAppointmentChange}>
+                  <option value="">Tidspunkt</option>
+                  {appointmentsBySpecialist[selectedSpecialist] && appointmentsBySpecialist[selectedSpecialist].map(appointment => (
+                    <option key={appointment.id} value={appointment.id}>{appointment.startTime} - {appointment.endTime}</option>
+                  ))}
+                </select>
+              </div>
+            )}
+
+            {selectedAppointment && (
+              <button type="submit">Bestill time</button>
+            )}
+          </form>
         </div>
-        {selectedSpecialist && (
-          <div>
-            <label>Velg avtaletidspunkt:</label>
-            <select value={selectedAppointment} onChange={handleAppointmentChange}>
-              <option value="">Tidspunkt</option>
-              {availableAppointments.map(appointment => (
-                <option key={appointment.id} value={appointment.id}>{appointment.startTime}</option>
-              ))}
-            </select>
-          </div>
-        )}
-        {selectedAppointment && (
-          <button type="submit">Bestill time</button>
-        )}
-      </form>
-    </div>
-    </div>
+      </div>
     </div>
   );
 }
