7b7c5ffbda50c731b155b26dfb1baaf1fe5acfa8
BenjaminOmar <benjaminjoof01@gmail.com>
2023-03-21 22:38:20 +0100

    updated flip

--- Athlete_Medical_sourcecode/Frontend/frontend-react/src/components/flip.js
+++ Athlete_Medical_sourcecode/Frontend/frontend-react/src/components/flip.js
@@ -1,28 +1,28 @@
-// //This is a React component that renders a video as the background of the home page of the web application.
+//This is a React component that renders a video as the background of the home page of the web application.
 
-// //Imports the React library.
-// import React, { useState } from "react";
-// import "./Flip.css";
-// import Card from "./Card/Card";
-// import { CSSTransition } from "react-transition-group";
+//Imports the React library.
+import React, { useState } from "react";
+import "./Flip.css";
+import Card from "./Card/Card";
+import { CSSTransition } from "react-transition-group";
 
-// //A function that returns a div container that contains the video and a text overlay.
-// function Flip() {
-// 	const [showFront, setShowFront] = useState(true);
+//A function that returns a div container that contains the video and a text overlay.
+function Flip() {
+	const [showFront, setShowFront] = useState(true);
 
-// 	return (
-// 		<div className="background">
-// 			<div className="flippable-card-container">
-// 				<CSSTransition in={showFront} timeout={300} classNames="flip">
-// 					<Card
-// 						onClick={() => {
-// 							setShowFront((v) => !v);
-// 						}}
-// 					/>
-// 				</CSSTransition>
-// 			</div>
-// 		</div>
-// 	);
-// }
-// //The component is exported as a default export.
-// export default Flip;
+	return (
+		<div className="background">
+			<div className="flippable-card-container">
+				<CSSTransition in={showFront} timeout={300} classNames="flip">
+					<Card
+						onClick={() => {
+							setShowFront((v) => !v);
+						}}
+					/>
+				</CSSTransition>
+			</div>
+		</div>
+	);
+}
+//The component is exported as a default export.
+export default Flip;
