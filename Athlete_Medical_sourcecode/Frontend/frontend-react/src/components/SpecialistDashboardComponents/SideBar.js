825598b272a6119a69ccca1f6c4a6795253e6845
Warsame <warsame9194@outlook.com>
2023-03-21 20:51:32 +0100

    update on the style

--- Athlete_Medical_sourcecode/Frontend/frontend-react/src/components/SpecialistDashboardComponents/SideBar.js
+++ Athlete_Medical_sourcecode/Frontend/frontend-react/src/components/SpecialistDashboardComponents/SideBar.js
@@ -6,26 +6,53 @@ import SideBarCSS from './SideBar.module.css';
 import { AiOutlineUser } from "react-icons/ai";
 
 function SideBar() {
+    const style = { color: "", fontSize: "2em" }
   return (
    <aside className={SideBarCSS.sidebarContainer}>
-         <div className={SideBarCSS.patient}> 
-         <div className={SideBarCSS.icons}><AiOutlineUser/></div>
-         <h1 className={SideBarCSS.title}>pasient</h1>
-         </div>  
-        <Link to='/' className={SideBarCSS.links}>finn pasient</Link><br></br>
-        <Link to='/' className={SideBarCSS.links}>finn avtalen</Link><br></br>
-        <div className={SideBarCSS.icons}><AiOutlineUser/></div>
-        <div className={SideBarCSS.journal}>
-        <h1 className={SideBarCSS.title}>journal</h1>
+         <div className={SideBarCSS.contentContainer}> 
+            <div className={SideBarCSS.titleWrapper}>
+                <div className={SideBarCSS.icons}>
+                    <AiOutlineUser style={style}/>
+                </div>
+                <div>
+                    <span className={SideBarCSS.title}>Pasient</span> 
+                </div>
+            </div>
+            <div className={SideBarCSS.linkWrapper}>
+                <Link to='/' className={SideBarCSS.links}>Finn Pasient</Link><br></br>
+                <Link to='/' className={SideBarCSS.links}>Finn Avtalen</Link><br></br>
+            </div>
         </div>
-        <Link to='/' className={SideBarCSS.links}>finn journal</Link><br></br>
-        <Link to='/' className={SideBarCSS.links}>skrive journal</Link><br></br>
-        <Link to='/' className={SideBarCSS.links}>oppdater journal</Link><br></br>
-        <div className={SideBarCSS.icons}><AiOutlineUser/></div>
-        <div className={SideBarCSS.schedule}>
-        <h1 className={SideBarCSS.title}>timeplan</h1>
+    
+        <div className={SideBarCSS.contentContainer}> 
+            <div className={SideBarCSS.titleWrapper}>
+                <div className={SideBarCSS.icons}>
+                    <AiOutlineUser style={style}/>
+                </div>
+                <div>
+                    <span className={SideBarCSS.title}>Journal</span> 
+                </div>
+            </div>
+            <div className={SideBarCSS.linkWrapper}>
+                <Link to='/' className={SideBarCSS.links}>Finn Journal</Link><br></br>
+                <Link to='/' className={SideBarCSS.links}>Skriv Journal</Link><br></br>
+                <Link to='/' className={SideBarCSS.links}>Endre Journal</Link><br></br>
+            </div>
         </div>
-        <Link to='/' className={SideBarCSS.links}>lever timeplan</Link>
+        <div className={SideBarCSS.contentContainer}> 
+            <div className={SideBarCSS.titleWrapper}>
+                <div className={SideBarCSS.icons}>
+                    <AiOutlineUser style={style}/>
+                </div>
+                <div>
+                    <span className={SideBarCSS.title}>Timeplan</span> 
+                </div>
+            </div>
+            <div className={SideBarCSS.linkWrapper}>
+                <Link to='/' className={SideBarCSS.links}>Lag Timeplan</Link><br></br>
+            </div>
+        </div>
+        
    </aside>
    
   );
