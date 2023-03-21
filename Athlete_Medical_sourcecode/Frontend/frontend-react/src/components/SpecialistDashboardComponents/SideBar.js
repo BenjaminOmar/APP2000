cc6506cfaff26d81555eee9220b2aac0eac7cff8
Warsame <warsame9194@outlook.com>
2023-03-21 15:07:47 +0100

    update

--- Athlete_Medical_sourcecode/Frontend/frontend-react/src/components/SpecialistDashboardComponents/SideBar.js
+++ Athlete_Medical_sourcecode/Frontend/frontend-react/src/components/SpecialistDashboardComponents/SideBar.js
@@ -3,19 +3,28 @@ import { Link } from 'react-router-dom';
 
 
 import SideBarCSS from './SideBar.module.css';
-
+import { AiOutlineUser } from "react-icons/ai";
 
 function SideBar() {
   return (
    <aside className={SideBarCSS.sidebarContainer}>
-        <div className={SideBarCSS.patient}>pasient</div>
+         <div className={SideBarCSS.patient}> 
+         <div className={SideBarCSS.icons}><AiOutlineUser/></div>
+         <h1 className={SideBarCSS.title}>pasient</h1>
+         </div>  
         <Link to='/' className={SideBarCSS.links}>finn pasient</Link><br></br>
         <Link to='/' className={SideBarCSS.links}>finn avtalen</Link><br></br>
-        <div className={SideBarCSS.journal}>journal</div>
+        <div className={SideBarCSS.icons}><AiOutlineUser/></div>
+        <div className={SideBarCSS.journal}>
+        <h1 className={SideBarCSS.title}>journal</h1>
+        </div>
         <Link to='/' className={SideBarCSS.links}>finn journal</Link><br></br>
         <Link to='/' className={SideBarCSS.links}>skrive journal</Link><br></br>
         <Link to='/' className={SideBarCSS.links}>oppdater journal</Link><br></br>
-        <div className={SideBarCSS.schedule}>timeplan</div>
+        <div className={SideBarCSS.icons}><AiOutlineUser/></div>
+        <div className={SideBarCSS.schedule}>
+        <h1 className={SideBarCSS.title}>timeplan</h1>
+        </div>
         <Link to='/' className={SideBarCSS.links}>lever timeplan</Link>
    </aside>
    
