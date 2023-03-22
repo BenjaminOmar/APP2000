feaf747b495b5df3e774f53eeb5b8c0036918548
BenjaminOmar <benjaminjoof01@gmail.com>
2023-03-22 21:11:11 +0100

    removed register button on header

--- Athlete_Medical_sourcecode/frontend/frontend-react/src/components/HeaderNormal.js
+++ Athlete_Medical_sourcecode/frontend/frontend-react/src/components/HeaderNormal.js
@@ -1,48 +1,51 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Button} from 'react-bootstrap';
-import './Header.css';
-import { Link, NavLink } from 'react-router-dom';
-import picture from '../images/footerImage2.png';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Button } from "react-bootstrap";
+import "./Header.css";
+import { Link, NavLink } from "react-router-dom";
+import picture from "../images/footerImage2.png";
 
+function HeaderNormal() {
+	return (
+		<Navbar className="navbar" expand="lg">
+			<Container>
+				<Navbar.Brand href="/" className="brand">
+					<img src={picture} alt="Logo" className={"logoAnimation"} />
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="ms-auto">
+						<NavLink to="/" className="list-item" activeClassName="active">
+							Hjem
+						</NavLink>
+						<NavLink
+							to="/services"
+							className="list-item"
+							activeClassName="active">
+							Tjenester
+						</NavLink>
+						<NavLink to="/" className="list-item" activeClassName="active">
+							Våre behandlere
+						</NavLink>
+						<NavLink
+							to="/contact"
+							className="list-item"
+							activeClassName="active">
+							Kontakt oss
+						</NavLink>
+					</Nav>
 
-
-
-function HeaderNormal () {
- 
-  return (
-    <Navbar className="navbar" expand="lg">
-      <Container>
-        <Navbar.Brand href="/" className='brand'> 
-        <img 
-          src={picture} 
-          alt='Logo'
-          className={"logoAnimation"}
-         
-        />
-        
-              </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            
-            <NavLink to='/' className='list-item' activeClassName='active'>Hjem</NavLink> 
-            <NavLink to='/services' className='list-item' activeClassName='active'>Tjenester</NavLink>
-            <NavLink to='/' className='list-item' activeClassName='active'>Våre behandlere</NavLink>
-            <NavLink to='/contact' className='list-item' activeClassName='active'>Kontakt oss</NavLink>
-            </Nav>
-            
-            <Nav className='ms-auto'>
-            <Link to='/register'> <Button className='btn btn-success'>Registrer</Button> </Link>     
-            <Link to='/login'> <Button className='btn btn-success'>Logg inn</Button> </Link>
-                  </Nav>
-            
-                 
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+					<Nav className="ms-auto">
+						<Link to="/login">
+							{" "}
+							<Button className="btn btn-success">Logg inn</Button>{" "}
+						</Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
 }
 
 export default HeaderNormal;
