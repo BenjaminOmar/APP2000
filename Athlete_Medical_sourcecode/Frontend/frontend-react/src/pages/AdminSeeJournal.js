162026a43b63b4e5369fd259e3cdc1c840dc5026
gunillag <83660232+gunillag@users.noreply.github.com>
2023-03-26 17:32:55 +0200

    adminseejournal

--- Athlete_Medical_sourcecode/Frontend/frontend-react/src/pages/AdminSeeJournal.js
+++ Athlete_Medical_sourcecode/Frontend/frontend-react/src/pages/AdminSeeJournal.js
@@ -1,53 +1,105 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Table, Container, Card } from 'react-bootstrap';
+import HeaderAdmin from "../components/HeaderAdmin";
+import Cookies from "js-cookie";
+import React, { useState } from "react";
+import axios from "axios";
+import { Button, Form, Table, Card, Alert } from "react-bootstrap";
 import '../components/SeeAllJournals.css';
 
-
 function AdminSeeJournal() {
-  const [journals, setJournals] = useState([]);
+  const username = Cookies.get("username");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-  useEffect(() => {
-    axios.get('https://localhost:7209/api/journal/getAll')
-      .then(response => {
-        setJournals(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
-
+  const searchJournals = async (event) => {
+    event.preventDefault();
+    try{
+      const response = await axios.get("https://localhost:7209/api/journal/getAll");
+      const data = response.data;
+      const filteredResults = data.filter(
+        (journal) =>
+          journal.journalnote1.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          journal.heading.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          journal.patient.toString().includes(searchTerm)
+      );
+      setSearchResults(filteredResults);
+    } catch (error) {
+      Alert(error);
+    };    
+  }  
 
   return (
-    <div className="AllJournals">
-    <div className="container d-flex justify-content-center">
-      <div className="box">
-        <h2>Oversikt over alle journalnotater</h2>
-        <Table striped bordered hover>
+    <>
+      <HeaderAdmin/>   
+      <div className="AllJournals">
+      <div className="container d-flex justify-content-center">
+        <div className="box">
+      <div style={{paddingTop: '30px', paddingBottom: '30px' }}>
+        <h2>Velkommen {username} </h2>
+      </div>  
+      <Card >
+        <Card.Header style={{textAlign: "center" }} as="h5">Søk i Journaler</Card.Header>
+        <Card.Body>
+          <Card.Text style={{paddingRight:'12%', paddingLeft: '12%'}} >
+            Her kan du søke etter en journal ved å skrive inn en tekst i søkefeltet under. Søket vil returnere alle journalnotater som inneholder den angitte teksten i tittel eller innhold. 
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Form.Group
+        className="mb-3"
+        style={{ marginLeft: "39%", marginRight: "40%" }}
+      >
+        <Form.Control
+          type="text"
+          placeholder="Søk etter en journal"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </Form.Group>
+      <Button
+        onClick={searchJournals}
+        style={{
+          marginLeft: "40%",
+          width: "19%",
+          marginBottom: "50px",
+        }}
+      >
+        Søk
+      </Button>
+
+      {searchResults.length > 0 && (
+        <Table
+          striped
+          bordered
+          hover
+          style={{ width: "70%", marginLeft: "12%", marginBottom: "100px" }}
+        >
           <thead>
-          <tr>
-              <th>Journalnote ID</th>
-                <th>Journalnotat</th>
-                <th>Tittel</th>
-                <th>Opprettet</th>
-                <th>Pasient</th>
-              </tr>
+            <tr>
+              <th>Journal ID</th>
+              <th>Journal Notat</th>
+              <th>Tittel</th>
+              <th>Opprettet</th>
+              <th>Pasient ID</th>
+            </tr>
           </thead>
           <tbody>
-              {journals.map(journal => (
-                <tr key={journal.journalnoteId}>
-                  <td>{journal.journalnoteId}</td>
-                  <td>{journal.journalnote}</td>
-                  <td>{journal.heading}</td>
-                  <td>{journal.created}</td>
-                  <td>{journal.patient}</td>
-                </tr>
-              ))}
-            </tbody>
+            {searchResults.map((journal) => (
+              <tr key={journal.journalnoteId}>
+                <td>{journal.journalnoteId}</td>
+                <td>{journal.journalnote1}</td>
+                <td>{journal.heading}</td>
+                <td>{new Date(journal.created).toLocaleString()}</td>
+
+                <td>{journal.patient}</td>
+              </tr>
+            ))}
+          </tbody>
         </Table>
+      )}
       </div>
-    </div>
-    </div>
+      </div>
+      </div>
+    </>
   );
 }
 
